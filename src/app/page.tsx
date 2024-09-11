7750d6989bcdc9afabebaf0e325244def91e2d5b
M-Shourav <muhammadshourav625@gmail.com>
2024-09-11 21:41:51 +0600

    banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Banner from "@/components/Banner";
 
 const HomePage = () => {
-  return <div className="py-10 bg-gray-500">HomePage</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,9 @@
 import logo from "@/assets/images/logo.png";
 import carticon from "@/assets/images/cartIcon.png";
-export { logo, carticon };
+import banner_1 from "@/assets/images/bannerOne.jpeg";
+import banner_2 from "@/assets/images/bannerTwo.jpeg";
+import banner_3 from "@/assets/images/bannerThree.jpeg";
+import banner_4 from "@/assets/images/bannerFour.jpeg";
+import banner_5 from "@/assets/images/bannerFive.jpeg";
+
+export { logo, carticon, banner_1, banner_2, banner_3, banner_4, banner_5 };
